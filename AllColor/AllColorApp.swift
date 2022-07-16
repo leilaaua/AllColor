0e1a1e8cd038637b1aa970bfe80ead56aabfda38
leilaaua <leila0777@icloud.com>
2022-07-16 16:35:20 +0300

    Initial Commit

---
+++ AllColor/AllColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllColorApp.swift
+//  AllColor
+//
+//  Created by leila on 16.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct AllColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
